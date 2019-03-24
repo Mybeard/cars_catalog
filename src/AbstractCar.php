e59ab18c324fa0d424a95e397d201be1dcc03f11
Oleksii Maiboroda <alexeymayboroda23@gmail.com>
2019-03-24 19:28:10 +0200

    usage add

--- src/AbstractCar.php
+++ src/AbstractCar.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the "default-project" package.
- *
- * (c) Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace App;
 
 class AbstractCar
